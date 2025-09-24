831ceda3bd9352fadf37e6b5ce961f1d75010900
santiago <santiagosiqueirasouto@gmail.com>
2025-09-24 11:29:42 -0300

    reset

--- scrapy_service/scrapy_service/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilitários compartilhados entre spiders, pipelines e middlewares."""
-
-from .context import bind_run_id, get_run_id, set_run_id
-
-__all__ = ["bind_run_id", "get_run_id", "set_run_id"]
